c7bacef9119c2b926e205e01307c10be2783247d
deepak <deepakbasari45@gmail.com>
2026-02-16 13:43:41 +0530

    sum of digits

---
+++ sum_looping/Sum_looping.java
@@ -0,0 +1,24 @@
+package sum_looping;
+
+import java.util.Scanner;
+
+public class Sum_looping {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+        num = Math.abs(num);
+
+        while (num != 0) {
+            int digit = num % 10;   // get last digit
+            sum = sum + digit;     // add digit to sum
+            num = num / 10;        // remove last digit
+        }
+
+        System.out.println("Sum of digits: " + sum);
+
+    }
+}
