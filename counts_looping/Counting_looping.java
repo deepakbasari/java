09829c3160d2b1e6de14c17e9ecfb21b61225fec
deepak <deepakbasari45@gmail.com>
2026-02-16 13:42:32 +0530

    counting the digits

---
+++ counts_looping/Counting_looping.java
@@ -0,0 +1,29 @@
+package counts_looping;
+
+import java.util.Scanner;
+
+public class Counting_looping {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        // Handle negative numbers
+        num = Math.abs(num);
+
+        if (num == 0) {
+            count = 1;
+        } else {
+            while (num != 0) {
+                num = num / 10;
+                count++;
+            }
+        }
+
+        System.out.println("Number of digits: " + count);
+
+    }
+}
